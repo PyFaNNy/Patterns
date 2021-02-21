e7ca7dec3938db28680ceae1f8e3cbf61ba33a09
Mr.Bubble <puzyrev.artyom@gmail.com>
2021-02-21 23:12:51 +0300

    реализация наивного одиночки

---
+++ Creational patterns/Singleton/NaiveSingleton.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Singleton
+{
+    class NaiveSingleton
+    {
+        private static NaiveSingleton _instance;
+        private NaiveSingleton() { }
+        public static NaiveSingleton GetInstance()
+        {
+            if (_instance == null)
+            {
+                _instance = new NaiveSingleton();
+            }
+            return _instance;
+        }
+        public void work()
+        {
+            Console.WriteLine("NaiveSingleton work!");
+        }
+    }
+}
---
+++ Creational patterns/Singleton/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            NaiveSingleton s1 = NaiveSingleton.GetInstance();
+            NaiveSingleton s2 = NaiveSingleton.GetInstance();
+
+            if (s1 == s2)
+            {
+                Console.WriteLine("NaiveSingleton works, both variables contain the same instance.");
+            }
+            else
+            {
+                Console.WriteLine("NaiveSingleton failed, variables contain different instances.");
+            }
+            s1.work();
+        }
+    }
+}
