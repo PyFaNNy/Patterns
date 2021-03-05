dafb12716218c40803d0a03cdd41e5cda1ba8800
Mr.Bubble <puzyrev.artyom@gmail.com>
2021-03-05 21:16:48 +0300

    добавил базовую реализацию строителя

---
+++ Creational patterns/Builder/Director.cs
@@ -0,0 +1,22 @@
+﻿namespace Builder
+{
+    class Director
+    {
+        private IBuilder _builder;
+
+        public IBuilder Builder
+        {
+            set { _builder = value; }
+        }
+        public void buildMinimalViableProduct()
+        {
+            this._builder.BuildPartA();
+        }
+        public void buildFullFeaturedProduct()
+        {
+            this._builder.BuildPartA();
+            this._builder.BuildPartB();
+            this._builder.BuildPartC();
+        }
+    }
+}
