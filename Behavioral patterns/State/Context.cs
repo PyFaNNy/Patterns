792c885b82bb062ecaff0a293a3f6c2c478c76d7
Mr.Bubble <puzyrev.artyom@gmail.com>
2021-03-21 02:24:00 +0300

    добавил базовую реализацию состояния

---
+++ Behavioral patterns/State/Context.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace State
+{
+    // Контекст определяет интерфейс, представляющий интерес для клиентов. Он
+    // также хранит ссылку на экземпляр подкласса Состояния, который отображает
+    // текущее состояние Контекста.
+    public class Context
+    {
+        // Ссылка на текущее состояние Контекста.
+        private State _state = null;
+
+        public Context (State state)
+        {
+            this.TransitionTo(state);
+        }
+        // Контекст позволяет изменять объект Состояния во время выполнения.
+        public void TransitionTo(State state)
+        {
+            Console.WriteLine($"Context: Transition to {state.GetType().Name}.");
+            this._state = state;
+            this._state.SetContext(this);
+        }
+        public void Request1()
+        {
+            this._state.Handle1();
+        }
+        public void Request2()
+        {
+            this._state.Handle2();
+        }
+    }
+}
---
+++ Behavioral patterns/State/State.cs
@@ -0,0 +1,18 @@
+﻿namespace State
+{
+    // Базовый класс Состояния объявляет методы, которые должны реализовать все
+    // Конкретные Состояния, а также предоставляет обратную ссылку на объект
+    // Контекст, связанный с Состоянием. Эта обратная ссылка может
+    // использоваться Состояниями для передачи Контекста другому Состоянию.
+    public abstract class State
+    {
+        protected Context _context;
+        public void SetContext(Context context)
+        {
+            this._context = context;
+        }
+        public abstract void Handle1();
+
+        public abstract void Handle2();
+    }
+}
